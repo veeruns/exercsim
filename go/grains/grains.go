ce8f9e1ac4ec885f311aca1148bdb7a17499990a
Veeru Natarajan <veeruns@yahoo.com>
2017-12-31 12:36:13 -0800

    Grains

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -3,18 +3,19 @@ package grains
 
 import (
 	"errors"
-	"math"
 )
 
+//Square returns a number 2 to the power of number -1
 func Square(input int) (uint64, error) {
 
 	if input <= 0 || input > 64 {
 		return 0, errors.New("Number cannot be zero or negative or greater than 64")
 	}
 
-	return uint64(math.Pow(float64(2), float64((input - 1)))), nil
+	return 1 << uint64(input-1), nil
 }
 
+//Total returns the total of squares from 1 to 64
 func Total() uint64 {
 	var sum uint64
 	for i := 1; i < 65; i++ {
