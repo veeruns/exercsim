53eb56a91407e6ba766ecd650cb2eefb3e41d0b5
Veeru Natarajan <veeruns@yahoo.com>
2018-01-09 12:01:51 -0800

    Using scoreboard structure

--- go/tournament/tournament.go
+++ go/tournament/tournament.go
@@ -1,115 +1,126 @@
-//package tournament implements Tournament
 package tournament
 
 import (
-	"bytes"
-	"errors"
+	"bufio"
 	"fmt"
 	"io"
 	"sort"
 	"strings"
 )
 
-func Tally(r io.Reader, buffer io.Writer) error {
-	p := make([]byte, 512)
-	r.Read(p)
-	tally := make(map[string][5]int)
-	line := strings.Split((string)(p), "\n")
-	for _, value := range line {
-		if value != "" {
-			match := strings.Split(value, ";")
-			if len(match) == 3 {
-				//fmt.Printf("Length of match is %s %d\n", match, len(match))
-				team1 := match[0]
-				team2 := match[1]
-				result := match[2]
-				if strings.Compare(result, "win") != 0 && strings.Compare(result, "loss") != 0 && strings.Compare(result, "draw") != 0 {
-					//fmt.Printf("Result is %s %d\n", result, strings.Compare(result, "win"))
-					return errors.New("Bad result")
-				}
+//TestVersion is the verion of the unit tests that this will pass
+const TestVersion = 2
 
-				if result == "win" {
-					c := tally[team1][0] + 1
-					win := tally[team1][1] + 1
-					drawn := tally[team1][2]
-					loss := tally[team1][3]
-					//points := win*3 + drawn*1
-					tally[team1] = [5]int{c, win, drawn, loss, 0}
-					cL := tally[team2][0] + 1
-					winL := tally[team2][1]
-					drawnL := tally[team2][2]
-					lossL := tally[team2][3] + 1
-					//pointsL := win*3 + drawn*1
-					tally[team2] = [5]int{cL, winL, drawnL, lossL, 0}
-				} else if result == "loss" {
-					c := tally[team1][0] + 1
-					win := tally[team1][1]
-					drawn := tally[team1][2]
-					loss := tally[team1][3] + 1
-					//points := win*3 + drawn*1
-					tally[team1] = [5]int{c, win, drawn, loss, 0}
-					cL := tally[team2][0] + 1
-					winL := tally[team2][1] + 1
-					drawnL := tally[team2][2]
-					lossL := tally[team2][3]
-					//pointsL := win*3 + drawn*1
-					tally[team2] = [5]int{cL, winL, drawnL, lossL, 0}
-				} else if result == "draw" {
-					c := tally[team1][0] + 1
-					win := tally[team1][1]
-					drawn := tally[team1][2] + 1
-					loss := tally[team1][3]
-					// := win*3 + drawn*1
-					tally[team1] = [5]int{c, win, drawn, loss, 0}
-					cL := tally[team2][0] + 1
-					winL := tally[team2][1]
-					drawnL := tally[team2][2] + 1
-					lossL := tally[team2][3]
-					//pointsL := win*3 + drawn*1
-					tally[team2] = [5]int{cL, winL, drawnL, lossL, 0}
-				}
-			}
+//scoreBoard keeps track of the score
+type scoreBoard map[string]*team
+
+//team is the win/loss record of the team
+type team struct {
+	name                            string
+	played, win, loss, draw, points int
+}
+
+/*Tally counts up the wins and losses for a sports season.*/
+func Tally(reader io.Reader, writer io.Writer) error {
+	scanner := bufio.NewScanner(reader)
+	board := make(scoreBoard)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" || strings.HasPrefix(line, "#") {
+			continue
+		}
+		if err := board.addGame(line); err != nil {
+			return err
 		}
 	}
-	//var retstring string
-	//retstring = append("Team                           | MP |  W |  D |  L |  P")
-	var b1 bytes.Buffer
-	//  retstring = append
 
-	for key, value := range tally {
-		points := value[1]*3 + value[2]*1
-		cL := tally[key][0]
-		winL := tally[key][1]
-		drawnL := tally[key][2]
-		lossL := tally[key][3]
-
-		tally[key] = [5]int{cL, winL, drawnL, lossL, points}
-
+	teams := board.getTeams()
+	sort.Sort(byScore(teams))
+	header := "Team                           | MP |  W |  D |  L |  P\n"
+	io.WriteString(writer, header)
+	for _, team := range teams {
+		io.WriteString(writer, team.String()+"\n")
 	}
-	hack := map[int]string{}
-	hackkeys := []int{}
-	for hkeys, val := range tally {
-		hack[val[4]] = hkeys
-		hackkeys = append(hackkeys, val[4])
-	}
-	sort.Sort(sort.Reverse(sort.IntSlice(hackkeys)))
-
-	b1.WriteString("Team                           | MP |  W |  D |  L |  P\n")
-	//op := fmt.Sprintf("%-31s|%2s|%2s|%2s|%2s|%2s\n", "Team", "MP", "W", "D", "L", "P")
-
-	//b1.WriteString(op)
-	for _, val := range hackkeys {
-		team := hack[val]
-		op := fmt.Sprintf("%-31s| %2d | %2d | %2d | %2d | %2d\n", team, tally[team][0], tally[team][1], tally[team][2], tally[team][3], tally[team][4])
-		b1.WriteString(op)
-	}
-	fmt.Println(b1.String())
-	io.WriteString(buffer, b1.String())
-	/*for key, value := range tally {
-		points := value[1]*3 + value[2]*1
-		op := fmt.Sprintf("%32s|%4d|%4d|%4d|%4d|%4d\n", key, value[0], value[1], value[2], value[3], points)
-		b1.WriteString(op)
-	}
-	fmt.Println(b1.String())*/
 	return nil
+}
+
+/*addGame validates and adds a game to the scoreboard.*/
+func (b scoreBoard) addGame(game string) error {
+	fields := strings.Split(game, ";")
+	if len(fields) != 3 {
+		return fmt.Errorf("Game not properly formatted: %s", game)
+	}
+	home, homeOk := b[fields[0]]
+	away, awayOk := b[fields[1]]
+	if !homeOk {
+		home = &team{name: fields[0]}
+		b[fields[0]] = home
+	}
+	if !awayOk {
+		away = &team{name: fields[1]}
+		b[fields[1]] = away
+	}
+	switch fields[2] {
+	case "win":
+		home.addWin()
+		away.addLoss()
+	case "loss":
+		home.addLoss()
+		away.addWin()
+	case "draw":
+		home.addDraw()
+		away.addDraw()
+	default:
+		return fmt.Errorf("Unknown win condition: %s", game)
+	}
+	return nil
+}
+
+/*getTeams converts the scoreboard into a list.*/
+func (b scoreBoard) getTeams() []team {
+	var teams []team
+	for _, team := range b {
+		teams = append(teams, *team)
+	}
+	return teams
+}
+
+//byScore sorts the teams by points, then by wins, then aphabetically
+type byScore []team
+
+func (t byScore) Len() int      { return len(t) }
+func (t byScore) Swap(i, j int) { t[i], t[j] = t[j], t[i] }
+func (t byScore) Less(i, j int) bool {
+	if t[i].points != t[j].points {
+		return t[i].points > t[j].points
+	} else if t[i].win != t[j].win {
+		return t[i].win > t[j].win
+	}
+	return t[i].name < t[j].name
+}
+
+/*addWin adds a win to the team.*/
+func (t *team) addWin() {
+	t.played++
+	t.win++
+	t.points += 3
+}
+
+/*addLoss adds a loss to the team.*/
+func (t *team) addLoss() {
+	t.played++
+	t.loss++
+}
+
+/*addDraw adds a draw to the team.*/
+func (t *team) addDraw() {
+	t.played++
+	t.draw++
+	t.points++
+}
+
+/*String gets the record of the team.*/
+func (t *team) String() string {
+	fmtString := "%-31s| %2d | %2d | %2d | %2d | %2d"
+	return fmt.Sprintf(fmtString, t.name, t.played, t.win, t.draw, t.loss, t.points)
 }
